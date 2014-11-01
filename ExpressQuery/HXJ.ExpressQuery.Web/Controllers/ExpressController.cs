530c6f6af2b757b43d0e5880078720217e6c0326
ColorWaterer <504562902@qq.com>
2014-11-01 16:01:54 +0800

    new

---
+++ ExpressQuery/HXJ.ExpressQuery.Web/Controllers/ExpressController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HXJ.ExpressQuery.Web.Controllers
+{
+    public class ExpressController : Controller
+    {
+        //
+        // GET: /Express/
+
+        /// <summary>
+        /// 首页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 调用API
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ExpressSendMsg()
+        {
+            return View();
+        }
+    }
+}
